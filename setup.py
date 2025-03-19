89d829fead2761e14813718ffc11a6b36362c714
Pooja Anilkumar <pujaa22@gmail.com>
2025-03-19 11:42:28 +0400

    Logging and exception

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .' # to automatically trigger requirements.txt 
 
-def get_requirements(file_path:str)->List[str]:
-    '''
-    This function will return the list of requirements
-    '''
+def get_requirements(file_path:str)->List[str]: # to get the libraries in requirements.txt
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=file_obj.readlines() # reading the list of library in reuirements.txt
+        requirements=[req.replace("\n","")for req in requirements] # replacing \n with blank while reading
         
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT) # while running the code, -e . is not required.
     
     return requirements
 
 
 setup(
-name="ML Projects",
+name="ML Project Automation",
 version="0.0.1",
 author="Pooja Anilkumar",
 author_email="pujaa22@gmail.com",
--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info() # out of three information, exc_tb is required for reading the error.
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error Occured in Python Script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error))

28d55dba043be64fbb4d5bd516f881d94d08c662
Pooja Anilkumar <pujaa22@gmail.com>
2025-03-24 01:18:02 +0400

    Deployment commit

--- application.py
+++ application.py
@@ -40,4 +40,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+    app.run(host="0.0.0.0")# during deployment debug=True need to be removed.     
